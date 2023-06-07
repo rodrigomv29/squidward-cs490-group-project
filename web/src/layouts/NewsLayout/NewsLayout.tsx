e4b59ecfc24c1106813e0e0040286d42fddeb9de
chrism1001 <christophermejia111@gmail.com>
2023-06-06 22:51:19 -0400

    creating basic layout

--- web/src/layouts/NewsLayout/NewsLayout.tsx
+++ web/src/layouts/NewsLayout/NewsLayout.tsx
@@ -1,9 +1,25 @@
+import { Link, routes } from '@redwoodjs/router'
+
 type NewsLayoutProps = {
   children?: React.ReactNode
 }
 
 const NewsLayout = ({ children }: NewsLayoutProps) => {
-  return <>{children}</>
+  return (
+    <>
+      <header>
+        <h1>Squidward News</h1>
+        <nav>
+          <ul>
+            <li>
+              <Link to={routes.about()}>About</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <main>{children}</main>
+    </>
+  )
 }
 
 export default NewsLayout
