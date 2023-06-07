49081a07c1f1fff35db31fa2d1516c47d3963721
ronaldcohen1611 <ronaldcohen16@gmail.com>
2023-06-07 05:07:47 -0400

    Added chakra-ui and custom button

--- web/src/layouts/NewsLayout/NewsLayout.tsx
+++ web/src/layouts/NewsLayout/NewsLayout.tsx
@@ -1,3 +1,5 @@
+import { Button } from '@chakra-ui/react'
+
 import { Link, routes } from '@redwoodjs/router'
 
 import SquidwardLogo from '../../../public/squidward_logo.png'
@@ -11,14 +13,18 @@ const NewsLayout = ({ children }: NewsLayoutProps) => {
     <>
       <header>
         <div className="main flex justify-between pt-3">
-          <div className="trending mx-4 text-lg font-extrabold">Trending</div>
+          <div className="trending mx-6 my-6 text-lg font-extrabold">
+            Trending
+          </div>
           <Link to={routes.home()}>
             <div className="main-logo-container flex">
               <div className="main-header-text flex items-center">
-                <div className="main-logo-squidward mx-2 h-10 rounded-md bg-emerald-400 px-3 py-1.5 text-2xl text-white">
+                <div className="main-logo-squidward mx-2 h-10 rounded-md bg-emerald-400 px-3 py-1.5 text-2xl font-extrabold text-white">
                   Squidward
                 </div>
-                <div className="main-logo-news text-4xl">News</div>
+                <div className="main-logo-news text-4xl font-semibold">
+                  News
+                </div>
               </div>
               <div className="squidward-logo">
                 <img
@@ -29,8 +35,10 @@ const NewsLayout = ({ children }: NewsLayoutProps) => {
               </div>
             </div>
           </Link>
-          <div className="sing-in-button mx-6 text-lg">
-            <Link to={routes.signIn()}>Sign In</Link>
+          <div className="sing-in-button mx-6 my-4 text-lg">
+            <Link to={routes.signIn()}>
+              <Button variant="custom_light">Sign in</Button>
+            </Link>
           </div>
         </div>
       </header>
