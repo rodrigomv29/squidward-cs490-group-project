37f20ffae9c367f92233ca65a8018bea7994273e
Christopher Mejia <58187990+chrism1001@users.noreply.github.com>
2023-06-13 14:43:40 -0400

    generating users sdl for login functionality

---
+++ api/src/services/users/users.scenarios.ts
@@ -0,0 +1,19 @@
+import type { Prisma, User } from '@prisma/client'
+import type { ScenarioData } from '@redwoodjs/testing/api'
+
+export const standard = defineScenario<Prisma.UserCreateArgs>({
+  user: {
+    one: {
+      data: { email: 'String872464', hashedPassword: 'String', salt: 'String' },
+    },
+    two: {
+      data: {
+        email: 'String5976645',
+        hashedPassword: 'String',
+        salt: 'String',
+      },
+    },
+  },
+})
+
+export type StandardScenario = ScenarioData<User, 'user'>
