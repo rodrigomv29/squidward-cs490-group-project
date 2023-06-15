e0ef05506427509c898eafaa27fdac27bd47c543
rodrigomv29 <43789876+rodrigomv29@users.noreply.github.com>
2023-06-15 18:56:18 -0400

    simple test written

---
+++ web/src/pages/LoginPage/LoginPage.test.tsx
@@ -0,0 +1,13 @@
+import { render } from '@redwoodjs/testing/web'
+import LoginPage from './LoginPage'
+/**
+ * insert a new test page for login function
+ */
+
+describe('LoginPage', () => {
+  it('renders successfully', () => {
+    expect(() => {
+      render(<LoginPage />)
+    }).not.toThrow()
+  })
+})
