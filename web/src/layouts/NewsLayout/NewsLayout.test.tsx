5f1b218996c666675d7f98dbd059333e54f1ac67
Ballslamic <71572589+Ballslamic@users.noreply.github.com>
2023-06-15 10:54:59 -0400

    Updated yarn and added tests for NewsLayouts
    Updates yarn to our standard 3.6.0, as well as added testing to make sure the navigation elements are present; and that certain on-action elements, such as clicking sign-in or the logo, will direct the user properly.

--- web/src/layouts/NewsLayout/NewsLayout.test.tsx
+++ web/src/layouts/NewsLayout/NewsLayout.test.tsx
@@ -1,14 +1,71 @@
-import { render } from '@redwoodjs/testing/web'
+import { render, screen, fireEvent } from '@redwoodjs/testing/web'
+
+import { MemoryRouter, Route } from 'react-router-dom'
+import '@testing-library/jest-dom/extend-expect'
 
 import NewsLayout from './NewsLayout'
-
-//   Improve this test with help from the Redwood Testing Doc:
-//   https://redwoodjs.com/docs/testing#testing-pages-layouts
+import { routes } from '@redwoodjs/router'
 
 describe('NewsLayout', () => {
   it('renders successfully', () => {
-    expect(() => {
-      render(<NewsLayout />)
-    }).not.toThrow()
+    render(
+      <MemoryRouter initialEntries={[routes.home()]}>
+        <Route path={routes.home()} element={<NewsLayout />} />
+      </MemoryRouter>
+    )
+
+    // Main logo
+    const logoLink = screen.getByText(/squidward/i)
+    expect(logoLink).toBeInTheDocument()
+
+    // Trending link
+    expect(screen.getByText(/trending/i)).toBeInTheDocument()
+
+    // Sign In / My Account
+    const signInLink = screen.getByText(/sign in/i)
+    expect(signInLink).toBeInTheDocument()
+
+    // Mobile menu
+    expect(screen.getByRole('button', { name: /mobile menu/i })).toBeInTheDocument()
+
+    // Navigation items
+    expect(screen.getByText(/home/i)).toBeInTheDocument()
+    expect(screen.getByText(/business/i)).toBeInTheDocument()
+    expect(screen.getByText(/entertainment/i)).toBeInTheDocument()
+    expect(screen.getByText(/health/i)).toBeInTheDocument()
+    expect(screen.getByText(/science/i)).toBeInTheDocument()
+    expect(screen.getByText(/sports/i)).toBeInTheDocument()
+    expect(screen.getByText(/technology/i)).toBeInTheDocument()
+    expect(screen.getByText(/search/i)).toBeInTheDocument()
+
+    // Footer
+    expect(screen.getByText(/redwoodjs/i)).toBeInTheDocument()
   })
+
+  it('navigates to the home page when logo is clicked', () => {
+    render(
+      <MemoryRouter initialEntries={[routes.home()]}>
+        <Route path={routes.home()} element={<NewsLayout />} />
+        <Route path={routes.home()} element={<div>Home Page</div>} />
+      </MemoryRouter>
+    )
+
+    const logoLink = screen.getByText(/squidward/i)
+    fireEvent.click(logoLink)
+    expect(screen.getByText(/home page/i)).toBeInTheDocument()
+  })
+
+  it('navigates to the sign-in page when Sign In link is clicked', () => {
+    render(
+      <MemoryRouter initialEntries={[routes.home()]}>
+        <Route path={routes.home()} element={<NewsLayout />} />
+        <Route path={routes.signIn()} element={<div>Sign In Page</div>} />
+      </MemoryRouter>
+    )
+
+    const signInLink = screen.getByText(/sign in/i)
+    fireEvent.click(signInLink)
+    expect(screen.getByText(/sign in page/i)).toBeInTheDocument()
+  })
+
 })
