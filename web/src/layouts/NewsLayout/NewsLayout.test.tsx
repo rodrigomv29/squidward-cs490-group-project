1395ad0684166fc1667b3cc61a1d9afe11d72321
ronaldcohen1611 <ronaldcohen16@gmail.com>
2023-06-15 12:08:46 -0400

    added sliding pannel component

--- web/src/layouts/NewsLayout/NewsLayout.test.tsx
+++ web/src/layouts/NewsLayout/NewsLayout.test.tsx
@@ -1,3 +1,5 @@
+import React from 'react'
+
 import { render } from '@redwoodjs/testing/web'
 
 import NewsLayout from './NewsLayout'
--- web/src/pages/DefaultHomePage/DefaultHomePage.tsx
+++ web/src/pages/DefaultHomePage/DefaultHomePage.tsx
@@ -1,24 +1,15 @@
 // import { Link, routes } from '@redwoodjs/router'
+import React from 'react'
+
 import { MetaTags } from '@redwoodjs/web'
+
+import SlidingPannel from '../../components/SlidingPannel/SlidingPannel'
 
 const DefaultHomePage = () => {
   return (
     <>
       <MetaTags title="DefaultHome" description="DefaultHome page" />
-      <div
-        className="main-container bg-blue-200"
-        style={{ height: '100vh', width: '99vw' }}
-      >
-        <div className="main-home-page bg-yellow-400">
-          <div className="main-header-containe w-64 bg-red-300">
-            <img
-              // src="https://wallpapers.com/images/hd/hd-sunset-at-beach-w5ped7x6g3uo2o76.jpg"
-              alt="Main-Header"
-              className=""
-            ></img>
-          </div>
-        </div>
-      </div>
+      <SlidingPannel />
     </>
   )
 }
