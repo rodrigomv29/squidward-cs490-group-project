814b4826eb043af1d6fdc6f4c3d79ce3878a096d
ronaldcohen1611 <ronaldcohen16@gmail.com>
2023-06-19 19:23:28 -0400

    added new menu from maertial ui

--- web/src/components/Weather/WeatherWidget.tsx
+++ web/src/components/Weather/WeatherWidget.tsx
@@ -1,10 +1,13 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, useContext } from 'react'
 
 import axios from 'axios'
 import { format } from 'date-fns'
 
+import CustomThemeContext from 'src/CustomThemeContext'
+
 const WeatherWidget = ({ city = 'Newark' }) => {
   const [weatherData, setWeatherData] = useState(null)
+  const { theme } = useContext(CustomThemeContext)
 
   useEffect(() => {
     const fetchData = async () => {
@@ -25,18 +28,34 @@ const WeatherWidget = ({ city = 'Newark' }) => {
   if (!weatherData) return <p>Loading...</p>
 
   return (
-    <div className="weather-container mx-30 my-4 mt-8 h-2/5 pt-6 text-center text-lg font-bold">
-      <h2 className="mb-2 rounded bg-emerald-400 p-2 text-3xl text-black">{`Weather in ${weatherData.location.name}, ${weatherData.location.region}`}</h2>
+    <div
+      className={`weather-container mx-30 my-4 mt-8 h-2/5 pt-6 text-center text-lg font-bold transition-colors duration-200 ${
+        theme === 1 ? 'text-white' : 'text-black'
+      }`}
+    >
+      <h2
+        className={`mb-2 rounded p-2 text-3xl transition-colors duration-200 ${
+          theme === 1 ? 'bg-gray-800 text-white' : 'bg-emerald-400 text-black'
+        }`}
+      >{`Weather in ${weatherData.location.name}, ${weatherData.location.region}`}</h2>
       <p className="text-2xl">{weatherData.current.condition.text}</p>
-      <p className="text-2xl">{`Current Temperature: ${Math.round(weatherData.current.temp_f)}°F`}</p>
+      <p className="text-2xl">{`Current Temperature: ${Math.round(
+        weatherData.current.temp_f
+      )}°F`}</p>
       <div className="forecast-container flex justify-around overflow-auto p-4">
         {weatherData.forecast.forecastday.slice(0, 3).map((day, index) => (
           <div
             key={index}
-            className="forecast-day m-2 flex flex-col items-center rounded bg-white p-4 text-center text-base text-black shadow"
+            className={`forecast-day m-2 flex flex-col items-center rounded p-4 text-center text-base text-black shadow transition-colors duration-200 ${
+              theme === 1
+                ? 'bg-gradient-to-br from-gray-700 to-emerald-400'
+                : 'bg-white'
+            }`}
           >
             <h4>{format(new Date(day.date), 'MMM dd')}</h4>
-            <p className="text-lg">{`High: ${Math.round(day.day.maxtemp_f)}°F, Low: ${Math.round(day.day.mintemp_f)}°F`}</p>
+            <p className="text-lg">{`High: ${Math.round(
+              day.day.maxtemp_f
+            )}°F, Low: ${Math.round(day.day.mintemp_f)}°F`}</p>
             <p className="text-lg">{day.day.condition.text}</p>
             <img src={day.day.condition.icon} alt="weather condition icon" />
           </div>
