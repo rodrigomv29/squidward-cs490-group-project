02fd201919c243bc95ec5a3ad1b40745cd326752
ronaldcohen1611 <ronaldcohen16@gmail.com>
2023-06-29 12:15:39 -0400

    added styling for category pages and highlighting for selecting tabs

--- web/src/components/TopNewsByCategory/TopNewsByCategory.tsx
+++ web/src/components/TopNewsByCategory/TopNewsByCategory.tsx
@@ -10,14 +10,16 @@ function TopNewsByCategory(articlesArray: { articles }) {
 
   const articles = articlesArray != null && articlesArray.articles.slice(10, 15)
 
+  console.log(articles)
+
   return (
     <div className="flex max-h-full flex-col">
       <div className="categories-container flex h-full flex-grow flex-col justify-between px-4 py-8">
         <div className="items-controller  max-h-full">
           <div className="main-container overflow-auto ">
             {articles &&
-              articles.map((article, index) => (
-                <div key={article.title} className="mb-2 flex flex-wrap">
+              articles.map((category, index) => (
+                <div key={category.name} className="mb-2 flex flex-wrap">
                   <span
                     className={`relative flex w-1/5 flex-col items-center justify-center text-3xl font-bold transition-colors duration-200 ${
                       theme === 1 ? 'text-white' : 'text-black'
@@ -28,6 +30,9 @@ function TopNewsByCategory(articlesArray: { articles }) {
                   </span>
 
                   <div className="category-container flex w-4/5 flex-col items-center justify-center py-2">
+                    <span className="flex justify-center text-left font-bold text-emerald-400">
+                      {category.name}
+                    </span>
                     <Link to={routes.home()}>
                       <span className="flex justify-center px-4">
                         <span
@@ -35,9 +40,9 @@ function TopNewsByCategory(articlesArray: { articles }) {
                             theme === 1 ? 'text-white' : 'text-black'
                           }`}
                         >
-                          {article.description.length > 200
-                            ? article.description.slice(0, 200) + '...'
-                            : article.description}
+                          {category && category.description.length > 200
+                            ? category.description.slice(0, 200) + '...'
+                            : category.description}
                         </span>
                       </span>
                     </Link>
