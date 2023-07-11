8e193ab7a5ce922f2d1d5d97abda9d6a4fcdde1d
billyr06 <billyramirez06@yahoo.com>
2023-07-11 18:22:05 -0400

    fixed styling and added 14 day option

--- web/src/pages/SearchResultsPage/SearchResultsPage.tsx
+++ web/src/pages/SearchResultsPage/SearchResultsPage.tsx
@@ -1,14 +1,6 @@
 import React, { useEffect, useState, useContext } from 'react'
 
-import {
-  Box,
-  Heading,
-  Image,
-  Text,
-  Select,
-  CSSReset,
-  ChakraProvider,
-} from '@chakra-ui/react'
+import { Box, Heading, Image, Text, Select } from '@chakra-ui/react'
 import { CircularProgress } from '@mui/material'
 import axios from 'axios'
 
@@ -72,6 +64,8 @@ const SearchResultsPage = () => {
               date.setDate(date.getDate() - 1);
             } else if (timeFilter === 'week') {
               date.setDate(date.getDate() - 7);
+            } else if (timeFilter === 'fortnight') {
+              date.setDate(date.getDate() - 14);
             } else if (timeFilter === 'month') {
               date.setMonth(date.getMonth() - 1);
             }
@@ -99,160 +93,153 @@ const SearchResultsPage = () => {
   })
 
   return (
-    <ChakraProvider resetCSS>
-      <NewsLayout>
-        <Box py={8} bgColor={handleMainTheme('gray.600', 'white')}>
-          <Heading
-            as="h1"
-            fontSize="6xl"
-            fontWeight="bold"
-            mb={8}
+    <NewsLayout>
+      <Box py={8} bgColor={handleMainTheme('gray.600', 'white')}>
+        <Heading
+          as="h1"
+          fontSize="6xl"
+          fontWeight="bold"
+          mb={8}
+          display="flex"
+          justifyContent="center"
+        >
+          <span className={handleTheme('text-white', 'text-black')}>
+            Search Results
+          </span>
+        </Heading>
+        {isLoading ? (
+          <Box
+            sx={{
+              display: 'flex',
+              justifyContent: 'center',
+              position: 'relative',
+              height: '100%',
+              color: '#34D399',
+              alignItems: 'center',
+            }}
+            className="flex flex-col"
+          >
+            <div className="mb-96">
+              <CircularProgress size={150} sx={{ color: '#34D399' }} />
+              <p className="font-lg pt-2 font-bold ">Loading Search Results</p>
+            </div>
+          </Box>
+        ) : searchResults.length === 0 ? (
+          <Box
+            h="screen"
             display="flex"
+            flexDirection="column"
+            alignItems="center"
             justifyContent="center"
           >
-            <span className={handleTheme('text-white', 'text-black')}>
-              Search Results
-            </span>
-          </Heading>
-          {isLoading ? (
-            <Box
-              sx={{
-                display: 'flex',
-                justifyContent: 'center',
-                position: 'relative',
-                height: '100%',
-                color: '#34D399',
-                alignItems: 'center',
-              }}
-              className="flex flex-col"
-            >
-              <div className="mb-96">
-                <CircularProgress size={150} sx={{ color: '#34D399' }} />
-                <p className="font-lg pt-2 font-bold ">Loading Search Results</p>
-              </div>
+            <Image
+              src="https://media1.tenor.com/images/2025c85773b942247e4565847e43a5d0/tenor.gif?itemid=7619217"
+              alt="No results found"
+              maxW={400}
+              mx="auto"
+              mb={8}
+            />
+            <Box textAlign="center">
+              <Heading as="h2" fontSize="4xl" fontWeight="bold" mb={4}>
+                No results found.
+              </Heading>
+              <Text fontSize="xl" mb={4}>
+                Please try a different search term.
+              </Text>
             </Box>
-          ) : searchResults.length === 0 ? (
-            <Box
-              h="screen"
-              display="flex"
-              flexDirection="column"
-              alignItems="center"
-              justifyContent="center"
-            >
-              <Image
-                src="https://media1.tenor.com/images/2025c85773b942247e4565847e43a5d0/tenor.gif?itemid=7619217"
-                alt="No results found"
-                maxW={400}
-                mx="auto"
-                mb={8}
-              />
-              <Box textAlign="center">
-                <Heading as="h2" fontSize="4xl" fontWeight="bold" mb={4}>
-                  No results found.
-                </Heading>
-                <Text fontSize="xl" mb={4}>
-                  Please try a different search term.
-                </Text>
+          </Box>
+        ) : (
+          <Box marginLeft={0} fontFamily="Arvo">
+            <Box mb={4} display="flex" justifyContent="center">
+              <Box w="200px" mr="4">
+                <Text mb="2" fontWeight="bold">Sort</Text>
+                <Select
+                  value={sortOrder}
+                  onChange={handleSortChange}
+                  w="100%"
+                  sx={{ appearance: 'none' }}
+                >
+                  <option value="latest">Latest</option>
+                  <option value="oldest">Oldest</option>
+                </Select>
+              </Box>
+              <Box w="200px" ml="4">
+                <Text mb="2" fontWeight="bold">Dates</Text>
+                <Select
+                  value={timeFilter}
+                  onChange={handleTimeFilterChange}
+                  w="100%"
+                  sx={{appearance: 'none'}}
+                >
+                  <option value="all">All Time</option>
+                  <option value="today">Today</option>
+                  <option value="week">Past 7 Days</option>
+                  <option value="fortnight">Past 14 Days</option>
+                  <option value="month">Past 30 Days</option>
+                </Select>
               </Box>
             </Box>
-          ) : (
-            <Box marginLeft={0} fontFamily="Arvo">
-              <Box mb={4} display="flex" justifyContent="center">
-                <Box w="200px" mr="4">
-                  <Text mb="2" fontWeight="bold">Sort</Text>
-                  <Select
-                    value={sortOrder}
-                    onChange={handleSortChange}
-                    w="100%"
-                    sx={{appearance: 'none'}}
-                  >
-                    <option value="latest">Latest</option>
-                    <option value="oldest">Oldest</option>
-                  </Select>
-                </Box>
-                <Box w="200px" ml="4">
-                  <Text mb="2" fontWeight="bold">Dates</Text>
-                  <Select
-                    value={timeFilter}
-                    onChange={handleTimeFilterChange}
-                    w="100%"
-                    sx={{appearance: 'none'}}
-                  >
-                    <option value="all">All Time</option>
-                    <option value="today">Today</option>
-                    <option value="week">Past 7 Days</option>
-                    <option value="month">Past 30 Days</option>
-                  </Select>
-                </Box>
-              </Box>
-              {sortedResults.map((result) => (
-                <Box key={result.url} mb={4}>
-                  <div className="items-cetner flex h-[300px] justify-center">
-                    <div className="my-10 flex w-[75%] flex-row">
-                      <div className="w-[30%] ">
-                        <img
-                          src={result.urlToImage}
-                          alt=""
-                          className="h-full w-full"
-                        />
-                      </div>
-                      <div className="flex w-[70%] flex-col p-8">
-                        <span
-                          className={`mb-8 text-lg font-bold hover:underline ${handleTheme(
-                            'text-white',
-                            'text-black'
-                          )}`}
+            {sortedResults.map((result) => (
+              <Box key={result.url} mb={4}>
+                <div className="items-cetner flex h-[300px] justify-center">
+                  <div className="my-10 flex w-[75%] flex-row">
+                    <div className="w-[30%] ">
+                      <img
+                        src={result.urlToImage}
+                        alt=""
+                        className="h-full w-full"
+                      />
+                    </div>
+                    <div className="flex w-[70%] flex-col p-8">
+                      <span
+                        className={`mb-8 text-lg font-bold hover:underline ${handleTheme(
+                          'text-white',
+                          'text-black'
+                        )}`}
+                      >
+                        <a
+                          href={result.url}
+                          rel="noreferrer"
+                          target="_blank"
+                          className="hover:text-blue-400"
                         >
-                          <a
-                            href={result.url}
-                            key={result.title}
-                            target="_blank"
-                            rel="noreferrer"
-                          >
-                            {result.title}
-                          </a>
-                        </span>
-                        <span>
-                          <span
-                            className={`${handleTheme(
-                              'text-white',
-                              'text-black'
-                            )}`}
-                          >
-                            {result?.description}
-                          </span>
-                        </span>
-                        <div className="bg-green- flex flex-row justify-between px-4 py-4">
-                          <Text fontSize="md" color="gray.800" fontWeight="bold">
-                            <span
-                              className={`${handleTheme(
-                                'text-white',
-                                'text-black'
-                              )}`}
-                            >
-                              Published on{' '}
-                              {new Date(result.publishedAt).toLocaleDateString()}
-                            </span>
-                          </Text>
-                          <div
-                            className={`font-bold ${handleTheme(
-                              'text-emerald-400',
-                              'text-black'
-                            )}`}
-                          >
-                            {result.source.name}
-                          </div>
-                        </div>
-                      </div>
+                          {result.title}
+                        </a>
+                      </span>
+                      <span
+                        className={`mb-4 ${handleTheme(
+                          'text-gray-400',
+                          'text-gray-500'
+                        )}`}
+                      >
+                        {new Date(result.publishedAt).toLocaleDateString()} |
+                        {result.source.name}
+                      </span>
+                      <p
+                        className={`text-justify mb-4 ${handleTheme(
+                          'text-white',
+                          'text-black'
+                        )}`}
+                      >
+                        {result.description}
+                      </p>
+                      <a
+                        href={result.url}
+                        rel="noreferrer"
+                        target="_blank"
+                        className="underline hover:text-blue-400"
+                      >
+                        Read more...
+                      </a>
                     </div>
                   </div>
-                </Box>
-              ))}
-            </Box>
-          )}
-        </Box>
-      </NewsLayout>
-    </ChakraProvider>
+                </div>
+              </Box>
+            ))}
+          </Box>
+        )}
+      </Box>
+    </NewsLayout>
   )
 }
 
